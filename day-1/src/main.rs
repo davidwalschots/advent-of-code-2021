15a4361758e44ce30dc972964e06fedac78b96cf
David Walschots <davidwalschots@users.noreply.github.com>
2021-12-05 19:05:18 +0100

    day 1 part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,14 +1,28 @@
 fn main() -> std::io::Result<()> {
-    println!("{}", number_of_increases(input_values()));
+    let input_values = input_values();
+    println!("increases: {}", number_of_increases(&input_values));
+    println!(
+        "sliding increases: {}",
+        number_of_sliding_window_increases(&input_values)
+    );
 
     Ok(())
 }
 
-fn number_of_increases(values: Vec<u32>) -> usize {
+fn number_of_increases(values: &Vec<u32>) -> usize {
     values
         .windows(2)
         .filter(|window| window[1] > window[0])
         .count()
+}
+
+fn number_of_sliding_window_increases(values: &Vec<u32>) -> usize {
+    let sliding_window_values: Vec<u32> = values
+        .windows(3)
+        .map(|window| window[0] + window[1] + window[2])
+        .collect();
+
+    number_of_increases(&sliding_window_values)
 }
 
 fn input_values() -> Vec<u32> {
@@ -20,14 +34,23 @@ fn input_values() -> Vec<u32> {
 
 #[cfg(test)]
 mod tests {
-    use super::number_of_increases;
+    use super::*;
 
     #[test]
     fn counts_number_of_increases() {
         let input: [u32; 10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        let result = number_of_increases(input.to_vec());
+        let result = number_of_increases(&input.to_vec());
 
         assert_eq!(result, 7);
+    }
+
+    #[test]
+    fn counts_number_of_increases_in_sliding_window() {
+        let input: [u32; 10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        let result = number_of_sliding_window_increases(&input.to_vec());
+
+        assert_eq!(result, 5);
     }
 }
